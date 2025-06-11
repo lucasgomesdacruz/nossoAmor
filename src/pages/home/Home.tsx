34aafe4d9a7d3517844d898c7ab5ca4734bfa640
Lucas Gomes <lucaass1997rp@gmail.com>
2025-06-11 18:03:28 -0300

    fix: erro de importação

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 
 import Hero from './hero/Hero'
 import Stories from './history/Stories'
-import Footer from '../../components/footer/Footer.jsx'
+import Footer from '../../components/footer/Footer'
 
 const Home = () => {
   return (
